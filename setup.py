9d7af82ca1653a5cad05ba68e963db7267beaf74
Asaf Silman <Asaf.Silman@health.wa.gov.au>
2018-07-24 17:14:52 +0800

    Updated dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ setup(
     version="0.1",
     packages=find_packages(),
 
+    install_requires=[
+        "click==6.7",
+        "PyYAML==3.13",
+        "pandas==0.23.3"
+    ],
+
     author="Asaf Silman",
     author_email="asaf.silman@health.wa.gov.au"
 )
