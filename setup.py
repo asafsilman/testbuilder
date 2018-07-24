17d490510a130df8f4ccf40cb48e80c8b6003016
Asaf Silman <Asaf.Silman@health.wa.gov.au>
2018-07-24 16:57:56 +0800

    Added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TestBuilder",
+    version="0.1",
+    packages=find_packages(),
+
+    author="Asaf Silman",
+    author_email="asaf.silman@health.wa.gov.au"
+)
