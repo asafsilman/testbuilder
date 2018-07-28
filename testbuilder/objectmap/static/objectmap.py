270f4ea017e8d8dbdded981caff029122818a7c8
Asaf Silman <asafsilman@gmail.com>
2018-07-28 20:57:23 +0800

    Added unittests for ObjectMapMiddleware

--- testbuilder/objectmap/static/objectmap.py
+++ testbuilder/objectmap/static/objectmap.py
@@ -1,6 +1,7 @@
 OBJECT_MAP = {
     "Sample": {
         "_title": "sample_title",
-        "sample_element": "basic://sample"
+        "sample_element": "basic://sample",
+        "sample_element_2": "basic://test/element"
     }
 }
