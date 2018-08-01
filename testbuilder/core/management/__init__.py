32e5072767cb22ac2511469451c359ec2f6630b3
Asaf Silman <asafsilman@gmail.com>
2018-08-01 19:34:46 +0800

    implemented iterations for running tests

--- testbuilder/core/management/__init__.py
+++ testbuilder/core/management/__init__.py
@@ -13,7 +13,9 @@ def run_test_case(file, file_type, profile):
 
     for test in tests:
         if test.run_test:
-            test.run()
+            for _ in range(test.test_iterations):
+                test.run()
+                test.configure_next_iteration()
 
 def run_unittests(verbose):
     package_tests = unittest.TestLoader().discover(start_dir="tests")
