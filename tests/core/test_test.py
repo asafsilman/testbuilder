1c00ece547afb0f2379208fdf0ed5931950762f1
Asaf Silman <asafsilman@gmail.com>
2018-08-08 20:12:39 +0800

    Added additional test for TBBaseTest

--- tests/core/test_test.py
+++ tests/core/test_test.py
@@ -1,10 +1,36 @@
 import unittest
 from testbuilder.core.base.basetest import TBBaseTest
-from testbuilder.core.base.basestep import TBBaseStep
+from testbuilder.core.base.basestep import TBBaseStep, StepContext
+from testbuilder.core.base.baseobjectmap import TBBaseObjectMap
+from testbuilder.core.base.basemiddleware import TBBaseMiddleware
 
-from testbuilder.core.exceptions import StepException
+from testbuilder.core.exceptions import StepException, ImproperlyConfigured
 from testbuilder.interface.basic.interface import BasicInterface
 from testbuilder.middleware.basic.middleware import BasicMiddleware
+
+from testbuilder.core.base.basetest import (
+    MIDDLEWARE_MODE_AFTER_STEP,
+    MIDDLEWARE_MODE_BEFORE_STEP,
+    MIDDLEWARE_MODE_STEP_FAILURE,
+    MIDDLEWARE_MODE_TEARDOWN_AFTER_STEP,
+    MIDDLEWARE_MODE_TEARDOWN_BEFORE_STEP
+)
+
+class SampleMiddleware(TBBaseMiddleware):
+    def after_step(self, step_context):
+        step_context.additional_settings["MiddlewareRun"] = "after_step"
+
+    def before_step(self, step_context):
+        step_context.additional_settings["MiddlewareRun"] = "before_step"
+    
+    def step_failure(self, step_context):
+        step_context.additional_settings["MiddlewareRun"] = "step_failure"
+
+    def tear_down_after_step(self, step_context):
+        step_context.additional_settings["MiddlewareRun"] = "tear_down_after_step"
+
+    def tear_down_before_step(self, step_context):
+        step_context.additional_settings["MiddlewareRun"] = "tear_down_before_step"
 
 class TestTBBaseTest(unittest.TestCase):
     def setUp(self):
@@ -22,6 +48,11 @@ class TestTBBaseTest(unittest.TestCase):
 
         self.assertEqual(self.test.first_step, self.step_1, "First step is not loaded")
         self.assertEqual(self.test.current_step, self.step_1, "Current step is not loaded")
+
+    def test_add_tear_down_steps(self):
+        self.test.load_tear_down_steps(self.step_1)
+
+        self.assertEqual(self.test.tear_down_first_step, self.step_1, "First step is not loaded")
     
     def test_add_step_wrong(self):
         with self.assertRaises(StepException):
@@ -31,11 +62,63 @@ class TestTBBaseTest(unittest.TestCase):
         self.test.run_test=True
         self.assertFalse(self.test.ready())
 
-        self.test.interfaces["basic"] = BasicInterface()
+        self.test.load_interface(BasicInterface, "basic")
         self.assertFalse(self.test.ready())
 
-        self.test.middlewares.append(BasicMiddleware())
+        self.test.load_middleware(BasicMiddleware)
         self.assertFalse(self.test.ready())
 
         self.test.load_steps(self.step_1)
         self.assertTrue(self.test.ready())
+
+    def test_load_interface(self):
+        self.test.load_interface(BasicInterface, "basic")
+
+        with self.assertRaises(ImproperlyConfigured):
+            # Try to load middleware as interface
+            self.test.load_interface(BasicMiddleware, "basic_failure")
+
+    def test_load_middleware(self):
+        self.test.load_middleware(BasicMiddleware)
+
+        with self.assertRaises(ImproperlyConfigured):
+            # Try to load interface as middleware
+            self.test.load_middleware(BasicInterface)
+
+    def test_load_objectmap(self):
+        object_map = TBBaseObjectMap("objectmap")
+
+        self.test.load_object_map(object_map, "objectmap")
+
+        with self.assertRaises(ImproperlyConfigured):
+            # Try load middleware as objectmap
+            self.test.load_object_map(BasicMiddleware(), "objectmap")
+
+    def test_run_middlewares(self):
+        def create_new_context():
+            c_x = StepContext(self.test)
+            c_x.additional_settings["MiddlewareRun"] = None
+            return c_x
+        
+        self.test.load_middleware(SampleMiddleware)
+        
+        step_context = create_new_context()
+        self.test.run_middlewares(step_context, MIDDLEWARE_MODE_AFTER_STEP)
+        self.assertEqual(step_context.additional_settings["MiddlewareRun"], "after_step")
+
+        step_context = create_new_context()
+        self.test.run_middlewares(step_context, MIDDLEWARE_MODE_BEFORE_STEP)
+        self.assertEqual(step_context.additional_settings["MiddlewareRun"], "before_step")
+
+        step_context = create_new_context()
+        self.test.run_middlewares(step_context, MIDDLEWARE_MODE_STEP_FAILURE)
+        self.assertEqual(step_context.additional_settings["MiddlewareRun"], "step_failure")
+        
+        step_context = create_new_context()
+        self.test.run_middlewares(step_context, MIDDLEWARE_MODE_TEARDOWN_AFTER_STEP)
+        self.assertEqual(step_context.additional_settings["MiddlewareRun"], "tear_down_after_step")
+
+        step_context = create_new_context()
+        self.test.run_middlewares(step_context, MIDDLEWARE_MODE_TEARDOWN_BEFORE_STEP)
+        self.assertEqual(step_context.additional_settings["MiddlewareRun"], "tear_down_before_step")
+        
