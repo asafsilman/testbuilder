eba07456ee085335d9f588139be21bfb10ce7153
Asaf Silman <Asaf.Silman@health.wa.gov.au>
2018-08-10 14:09:53 +0800

    Updated setup.py with additional project information

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="testbuilder",
     version="0.2.3",
     packages=find_packages(),
+    description="A python testing framework for frontend testing",
 
     package_data = {
         '': ['*.csv', '*.yaml'],
@@ -16,6 +17,16 @@ setup(
         "selenium==3.13.0"
     ],
 
+    project_urls={
+        "Source": "https://github.com/AsafSilman/testbuilder",
+    },
+
+    classifiers=[
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python :: 3.7"
+    ],
+
     author="Asaf Silman",
     author_email="asaf.silman@health.wa.gov.au"
 )
+
